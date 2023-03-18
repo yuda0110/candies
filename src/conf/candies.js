db61c29a3cc60ff98b712085b6a31500631e197e
Yuko Uda <yuko.uda0110@gmail.com>
2023-03-18 14:58:55 -0700

    Create candies config file for candy list

---
+++ src/conf/candies.js
@@ -0,0 +1,82 @@
+export const candies = [
+  {
+    id: 0,
+    name: 'Dark Chocolate Bunny',
+    price: 9.00
+  },
+  {
+    id: 1,
+    name: 'Milk Chocolate Bunny',
+    price: 9.00
+  },
+  {
+    id: 2,
+    name: 'Sweet Surprise Chicks',
+    price: 12.00
+  },
+  {
+    id: 3,
+    name: 'Strawberry Cream Lollypops',
+    price: 9.50
+  },
+  {
+    id: 4,
+    name: 'Orange Cream Lollypops',
+    price: 9.50
+  },
+  {
+    id: 5,
+    name: 'Milk Chocolate Eggs',
+    price: 9.75
+  },
+  {
+    id: 6,
+    name: 'Dark Chocolate Eggs',
+    price: 9.75
+  },
+  {
+    id: 7,
+    name: 'Sour Bunnies',
+    price: 9.50
+  },
+  {
+    id: 8,
+    name: 'Jelly Bird Eggs',
+    price: 8.00
+  },
+  {
+    id: 9,
+    name: 'Jelly Beans',
+    price: 7.50
+  },
+  {
+    id: 10,
+    name: 'Café Latté Krispys',
+    price: 14.50
+  },
+  {
+    id: 11,
+    name: 'Peppermint Krispys',
+    price: 14.50
+  },
+  {
+    id: 12,
+    name: 'Lemon Drops',
+    price: 11.00
+  },
+  {
+    id: 13,
+    name: 'Peanut Brittle',
+    price: 9.50
+  },
+  {
+    id: 14,
+    name: 'Maple Cashew Brittle',
+    price: 18.00
+  },
+  {
+    id: 15,
+    name: 'Milk Chocolate Balls',
+    price: 9.75
+  }
+];

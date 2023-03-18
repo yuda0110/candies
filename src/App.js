be4323eabf0c0cf311b5533163dd2ff20eff8de9
Yuko Uda <yuko.uda0110@gmail.com>
2023-03-18 14:30:15 -0700

    Install node-sass and set up basic sass files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      Test
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './sass/main.scss';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';

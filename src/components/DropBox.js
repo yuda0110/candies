bc30026d6d170e9c89283e914499be53932db3df
Yuko Uda <yuko.uda0110@gmail.com>
2023-03-18 15:52:48 -0700

    Create DropBox component

---
+++ src/components/DropBox.js
@@ -0,0 +1,11 @@
+import styles from './DropBox.module.scss';
+
+function DropBox() {
+  return (
+    <div className={styles.dropBox}>
+      Drop Candy Here!
+    </div>
+  )
+}
+
+export default DropBox;
